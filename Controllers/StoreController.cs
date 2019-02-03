fcb77bf859e18bb1d87e5fcbde58acf51098cfad
Intern2 <intern2@swifttech.com>
2019-02-03 17:39:27 +0545

    First-commit

---
+++ Controllers/StoreController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MusicApplication.Models;
+
+namespace MusicApplication.Controllers
+{
+    public class StoreController : Controller
+    {
+
+        MusicStoreEntities storeDB = new MusicStoreEntities();
+        // GET: /Store/
+        public ActionResult Index()
+        {
+            var genres = storeDB.Genres.ToList();
+            return View(genres);
+        }
+
+        //Get: /Store/Browse
+        //public String Browse()
+        //{
+        //    return "Hello from Store.Browse()";
+        //}
+
+        //Get: /Store/Details
+        public ActionResult Details(int id)
+        {
+            var album = new Album { Title = "Album" + id };
+            return View(album);
+        }
+
+        //GET: /Store/Browse?genre=Disco
+
+        public ActionResult Browse(string genre)
+        {
+            var genreModel = new Genre { Name = genre };
+            return View(genreModel);
+        }
+    }
+}
